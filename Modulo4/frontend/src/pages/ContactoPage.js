9d687a02ca7b849d63c938078fab6bef4691767a
Juan Corbalan <juanmarcoscorbalan@hotmail.com>
2022-03-08 16:42:27 -0300

    Parte 2 de React, coloco estilos en lapagina

--- Modulo4/frontend/src/pages/ContactoPage.js
+++ Modulo4/frontend/src/pages/ContactoPage.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import '../styles/components/pages/ContactoPage.css'
 const ContactoPage = (props) => {
     return (
         <main className="holder">
-            <div className="columna contacto">
+            <div className="columna left">
                 <h2> Contacto Rapido </h2>
                 <form action="" method="" class="formulario">
                     <p>
@@ -25,7 +25,7 @@ const ContactoPage = (props) => {
                     <p className="centrar"><input type="submit" value="Enviar"/> </p>
                 </form>
             </div>
-            <div className="columna datos">
+            <div className="columna right">
                 <h2>Otras vias de contacto</h2>
                 <p>Tambien se pueden comunicar con nosotros ....</p>
                 <ul>
--- Modulo4/frontend/src/pages/RecomendacionesPage.js
+++ Modulo4/frontend/src/pages/RecomendacionesPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import '../styles/components/pages/RecomendacionesPage.css'
 const RecomendacionesPage = (props) => {
     return (
         <section className="holder">
